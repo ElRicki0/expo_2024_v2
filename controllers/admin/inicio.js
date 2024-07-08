5b952412fa256efc1db3238e5afd2210c87cb4c1
Juanpa3002 <106353452+AndyPineda1@users.noreply.github.com>
2024-07-08 11:24:41 -0600

    inicio page

---
+++ controllers/admin/inicio.js
@@ -0,0 +1,7 @@
+// Método del evento para cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', () => {
+    // Llamada a la función para mostrar el encabezado y pie del documento.
+    loadTemplate();
+    // Llamada a la función para llenar la tabla con los registros existentes.
+    // fillTable();
+});
