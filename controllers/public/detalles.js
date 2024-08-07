aa7bcfe7637ea9611d8aaaf818915c0736b19bdc
Ricardo Nicolas Melara <107507393+ElRicki0@users.noreply.github.com>
2024-08-07 11:42:23 -0600

    avance en detalles servicios

--- controllers/public/detalles.js
+++ controllers/public/detalles.js
@@ -1,6 +1,7 @@
 // Constantes para completar la ruta de la API.
-const PRODUCTO_API = 'services/public/producto.php';
-const PEDIDO_API = 'services/public/pedido.php';
+const SERVICIO_API = 'services/public/servicio.php';
+// const CITA_API ='services/public/servicio.php';
+
 // Constante tipo objeto para obtener los parámetros disponibles en la URL.
 const PARAMS = new URLSearchParams(location.search);
 // Constante para establecer el formulario de agregar un producto al carrito de compras.
@@ -11,4 +12,24 @@ const SHOPPING_FORM = document.getElementById('shoppingForm');
 document.addEventListener('DOMContentLoaded', async () => {
     // Llamada a la función para mostrar el encabezado y pie del documento.
     loadTemplate();
+    // Se establece el título del contenido principal.
+    MAIN_TITLE.textContent = 'Detalles servicio';
+    // Constante tipo objeto con los datos del producto seleccionado.
+    const FORM = new FormData();
+    FORM.append('idServicio', PARAMS.get('id'));
+    // Petición para solicitar los datos del producto seleccionado.
+    const DATA = await fetchData(SERVICIO_API, 'readOne', FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        // Se colocan los datos en la página web de acuerdo con el producto seleccionado previamente.
+        document.getElementById('imagenServicio').src = SERVER_URL.concat('images/servicios/', DATA.dataset.imagen_servicio);
+        document.getElementById('tipoServicio').textContent = DATA.dataset.tipo_servicio;
+        document.getElementById('descripcionServicio').textContent = DATA.dataset.descripcion_servicio;
+        document.getElementById('idServicio').value = DATA.dataset.id_servicio;
+    } else {
+        // Se presenta un mensaje de error cuando no existen datos para mostrar.
+        document.getElementById('mainTitle').textContent = DATA.error;
+        // Se limpia el contenido cuando no hay datos para mostrar.
+        document.getElementById('detalle').innerHTML = '';
+    }
 });
