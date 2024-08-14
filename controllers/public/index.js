9928839c648c133a963598d645ae2f138ae7bc22
stanlyy <20220127@ricaldone.edu.sv>
2024-08-14 12:10:52 -0600

    avance en index (diseño y logica)

--- controllers/public/index.js
+++ controllers/public/index.js
@@ -1,5 +1,11 @@
-// Método del evento para cuando el documento ha cargado.
-document.addEventListener('DOMContentLoaded', async () => {
-    // Llamada a la función para mostrar el encabezado y pie del documento.
-    loadTemplate();
-})
+// Constantes para completar las rutas de la API.
+const SERVICIO_API = 'services/admin/servicio.php';
+
+// Constantes para establecer el contenido de la tabla.
+const SERVICIOS = document.getElementById('servicios');
+
+    // Método del evento para cuando el documento ha cargado.
+    document.addEventListener('DOMContentLoaded', async () => {
+        // Llamada a la función para mostrar el encabezado y pie del documento.
+        loadTemplate();
+    })
