f04139ae397362289c6e1da4548cbced5505f406
Jhonny Amaya <136754303+Jhonnyy0@users.noreply.github.com>
2024-09-08 12:19:37 -0600

    Arreglo de titulos

--- controllers/utils/admin.js
+++ controllers/utils/admin.js
@@ -63,10 +63,10 @@ const loadTemplate = async () => {
                                 <a class="nav-link" href="clientes.html"><i class="bi bi-people-fill me-1"></i> Clientes</a>
                             </li>
                             <li class="nav-item ms-4">
-                                <a class="nav-link" href="empleados.html"><i class="bi bi-person-vcard-fill"></i> Empleados</a>
+                                <a class="nav-link" href="empleados.html"><i class="bi bi-person-vcard-fill me-1"></i> Empleados</a>
                             </li>
                             <li class="nav-item ms-4">
-                                <a class="nav-link" href="administradores.html"><i class="bi bi-shield-lock-fill"></i> Admin</a>
+                                <a class="nav-link" href="administradores.html"><i class="bi bi-shield-lock-fill me-1"></i> Admin</a>
                             </li>
                             <li class="nav-item ms-4">
                                 <a class="nav-link" href="servicios.html"><i class="bi bi-file-medical-fill me-1"></i> Servicios</a>
