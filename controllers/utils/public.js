b82a664c0c7e67147ebfff01cbbc547577b2827f
jhonnyy0 <jhonnyamaya168@gmail.com>
2024-10-07 11:19:14 -0600

    Cambios en footer

--- controllers/utils/public.js
+++ controllers/utils/public.js
@@ -104,5 +104,28 @@ const loadTemplate = async () => {
             </div>
         </div>
     </footer>
+    <div class="copyright-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-6 col-lg-6 text-center text-lg-left">
+                        <div class="copyright-text">
+                            <p>Copyright &copy; 2024, All Right Reserved <a
+                                    href="https://codepen.io/anupkumar92/">Here</a></p>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+                        <div class="footer-menu">
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Terms</a></li>
+                                <li><a href="#">Privacy</a></li>
+                                <li><a href="#">Policy</a></li>
+                                <li><a href="#">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     `);
 }
